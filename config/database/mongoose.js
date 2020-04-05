611b75c4184190c673a77c4dbd1f40ca11914dc8
Manoj Yocket <manojyocket@Manojs-MacBook-Pro.local>
2020-04-05 20:16:59 +0530

    Configuration

--- config/database/mongoose.js
+++ config/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 module.exports = {
     mongoose
